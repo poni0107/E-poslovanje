225d7785170aa2411457ada12f6d88c2662b0309
Sava01 <savadj.01@gmail.com>
2024-11-29 14:46:11 +0100

    Change import statement

--- epos/models.py
+++ epos/models.py
@@ -5,8 +5,7 @@
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
-import django.db
-
+from django.db import models
 
 class Accessories(models.Model):
     accessory_id = models.AutoField(primary_key=True)
